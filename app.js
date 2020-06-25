82c229cd875d6184f9b73da49fda09e3d73707f6
JordanCarlson7 <49733006+JordanCarlson7@users.noreply.github.com>
2020-06-25 15:48:37 -0600

    no query, non existing query

--- app.js
+++ app.js
@@ -5,34 +5,34 @@ var app = new express();
 
 
 app.set("port", (process.env.PORT || 5000));
-app.get("/", displayHomePage);
+app.get("/", function (req, res) {
+    res.send("just a homepage");
+})
+app.get("/getUser", getUser);
 
 //----------
 //Pooling
 const connectionString = process.env.DATABASE_URL || "postgress://localtester:localpassword@localhost:5432/affiliate_blogger";
-const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({ connectionString: connectionString });
 
-function displayHomePage(req, res) {
+function getUser(req, res) {
     console.log("Connected to the Homepage");
 
-if (req.query.id) {
-    var id =  req.query.id;
-    getPersonFromDb(id, function (error, result) {
-        if (error || result == null || result.length != 1) {
-            response.status(500).json({data: error});
-
-
-        }
-        else {
-        console.log("back grom data base with result", result);
-        res.json(result[0]);
-        }
-    });
-}
-else {
-    console.log("no query");
-}
-    
+    if (req.query.id) {
+        var id = req.query.id;
+        getPersonFromDb(id, function (error, result) {
+            if (error || result == null || result.length != 1) {
+                res.status(500).json({ data: error });
+            }
+            else {
+                console.log("back grom data base with result", result);
+                res.json(result[0]);
+            }
+        });
+    }
+    else {
+        console.log("no query");
+    }
 }
 
 function getPersonFromDb(id, callback) {
@@ -41,19 +41,25 @@ function getPersonFromDb(id, callback) {
     var sql = "SELECT * FROM users WHERE id = $1::int";
     var params = [id];
 
-    pool.query(sql, params, function (err, result) {
-        if (err || result === 'undefined') {
-            console.log("error in DB");
-            console.log(err);
-            callback(err, null);
-        }
-        else {
-        console.log("DB Result" + JSON.stringify(result.rows));
-        callback(null, result.rows); 
-        }
-    })
+    try {
+        pool.query(sql, params, function (err, result) {
+            if (err || result == 'undefined') {
+                console.log("error in DB");
+                console.log(err);
+                callback(err, null);
+            }
+            else {
+                console.log("DB Result" + JSON.stringify(result.rows));
+                callback(null, result.rows);
+            }
+        })
+    }
+    catch (err) {
+        console.log("Attempted connection but...", err);
+        callback(err, null);
+    }
 }
 
-app.listen(app.get("port"), function() {
+app.listen(app.get("port"), function () {
     console.log("Listening on port:" + app.get("port"));
 });
